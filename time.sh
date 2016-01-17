43ccf4149d33e70a8d41a701e51d9d05af1b3596
Jason Yao <jasony.edu@gmail.com>
2016-01-17 10:15:29 -0500

    Fixed time script to actually run

--- time.sh
+++ time.sh
@@ -5,7 +5,7 @@ set -e
 # Write out current crontab
 crontab -l > tempCron
 
-echo "30 0 * * sat /server/minutes_emailer/run.sh" >> tempCron
+echo "30 0 * * sat /bin/bash /server/minutes_emailer/run.sh" >> tempCron
 
 # Adds a newline character
 echo  >> tempCron;
